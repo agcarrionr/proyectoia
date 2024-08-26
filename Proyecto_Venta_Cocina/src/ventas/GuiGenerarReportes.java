9d591fe935b19355b2ecda531b367fe90ec922c4
Anghi Rodriguez <anghi.rodriguez@kabeli.cl>
2024-08-25 21:01:20 -0400

    Diseño Reportes

--- Proyecto_Venta_Cocina/src/ventas/GuiGenerarReportes.java
+++ Proyecto_Venta_Cocina/src/ventas/GuiGenerarReportes.java
@@ -7,11 +7,22 @@ import javax.swing.JButton;
 import javax.swing.JDialog;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import javax.swing.JComboBox;
+import java.awt.Font;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.JScrollPane;
+import javax.swing.DefaultComboBoxModel;
 
-public class GuiGenerarReportes extends JDialog {
+public class GuiGenerarReportes extends JDialog implements ActionListener {
 
 	private static final long serialVersionUID = 1L;
 	private final JPanel contentPanel = new JPanel();
+	private JLabel lblTipoReporte;
+	private JComboBox cboTipoRepor;
+	private JButton btnCerrar;
+	private JScrollPane spReportes;
 
 	/**
 	 * Launch the application.
@@ -30,27 +41,43 @@ public class GuiGenerarReportes extends JDialog {
 	 * Create the dialog.
 	 */
 	public GuiGenerarReportes() {
-		setBounds(100, 100, 450, 300);
+		setTitle("Generar Reportes");
+		setBounds(100, 100, 650, 454);
 		getContentPane().setLayout(new BorderLayout());
-		contentPanel.setLayout(new FlowLayout());
 		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
 		getContentPane().add(contentPanel, BorderLayout.CENTER);
+		contentPanel.setLayout(null);
+		
+		lblTipoReporte = new JLabel("Tipo de reporte");
+		lblTipoReporte.setFont(new Font("Lucida Grande", Font.PLAIN, 15));
+		lblTipoReporte.setBounds(25, 36, 119, 19);
+		contentPanel.add(lblTipoReporte);
+		
+		cboTipoRepor = new JComboBox();
+		cboTipoRepor.setModel(new DefaultComboBoxModel(new String[] {"Seleccionar", "Ventas por modelo", "Promedios menores y mayores", "Precios en relación al precio promedio", "Venta en relación a la venta óptima"}));
+		cboTipoRepor.setBounds(139, 30, 304, 34);
+		contentPanel.add(cboTipoRepor);
+		
+		btnCerrar = new JButton("Cerrar");
+		btnCerrar.addActionListener(this);
+		btnCerrar.setBounds(493, 33, 117, 29);
+		contentPanel.add(btnCerrar);
+		
+		spReportes = new JScrollPane();
+		spReportes.setBounds(49, 75, 531, 323);
+		contentPanel.add(spReportes);
 		{
 			JPanel buttonPane = new JPanel();
 			buttonPane.setLayout(new FlowLayout(FlowLayout.RIGHT));
 			getContentPane().add(buttonPane, BorderLayout.SOUTH);
-			{
-				JButton okButton = new JButton("OK");
-				okButton.setActionCommand("OK");
-				buttonPane.add(okButton);
-				getRootPane().setDefaultButton(okButton);
-			}
-			{
-				JButton cancelButton = new JButton("Cancel");
-				cancelButton.setActionCommand("Cancel");
-				buttonPane.add(cancelButton);
-			}
 		}
 	}
-
+	public void actionPerformed(ActionEvent e) {
+		if (e.getSource() == btnCerrar) {
+			actionPerformedBtnNewButtonJButton(e);
+		}
+	}
+	protected void actionPerformedBtnNewButtonJButton(ActionEvent e) {
+		dispose();
+	}
 }
